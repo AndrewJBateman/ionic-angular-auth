e798c8ca3137653f2f58e148ba89cdc3f18cd77d
AndrewJBateman <gomezbateman@yahoo.com>
2022-01-20 13:41:44 +0100

    chore: updated to Angular v13

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,14 +12,13 @@ import { IonicStorageModule } from '@ionic/storage-angular';
 import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
-	declarations: [AppComponent],
-	entryComponents: [],
-	imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, HttpClientModule, IonicStorageModule.forRoot()],
-	providers: [
-		StatusBar,
-		SplashScreen,
-		{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }
-	],
-	bootstrap: [AppComponent]
+    declarations: [AppComponent],
+    imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, HttpClientModule, IonicStorageModule.forRoot()],
+    providers: [
+        StatusBar,
+        SplashScreen,
+        { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }
+    ],
+    bootstrap: [AppComponent]
 })
 export class AppModule { }
